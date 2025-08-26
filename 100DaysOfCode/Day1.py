56d4411c5658372503dfe383a26eaa3c66f72e7f
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-08-26 14:05:44 -0400

    completed print, string and input

---
+++ 100DaysOfCode/Day1.py
@@ -0,0 +1,13 @@
+# Printing to the Console
+print("Hello World")
+#-------------------------------------------------------------------------------
+# String Manipulation
+print("Hello World \nMy name is Bahsoun \nI am looking to get my Python down")
+# can use + to concatonate strings
+print("Mohamad" + " " + "Bahsoun")
+#-------------------------------------------------------------------------------
+# getting input from the user
+name = input("What is your name?\n")
+print("The users name is" + " " + name + "!")
+
+
