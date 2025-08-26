02a245055c84e432411cc5746f643085291e38a8
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-08-26 14:36:00 -0400

    completed Day1, starting Day 2

--- 100DaysOfCode/Day1.py
+++ 100DaysOfCode/Day1.py
@@ -7,7 +7,17 @@ print("Hello World \nMy name is Bahsoun \nI am looking to get my Python down")
 print("Mohamad" + " " + "Bahsoun")
 #-------------------------------------------------------------------------------
 # getting input from the user
-name = input("What is your name?\n")
-print("The users name is" + " " + name + "!")
+print("The users name is" + " " + input("What is your name?\n") + "!")
+#-------------------------------------------------------------------------------
+# python variables
+fruit = input("What is your favorite fruit?\n")
+print("You chose the" + fruit + " Devil Fruit\nAnd there are " + str(len(fruit)) +" characters")
+#-------------------------------------------------------------------------------
+# Naming Variables - can pretty much be anything but there are rules
+# 1) make the variables names are readable and related to the code
+# 2) seperate multiple words with a underscore
+# 3) you can use numbers but they cannot be at the start
+# 4) can use names that already exists as predifned funcitons like for, while, print etc
+#-------------------------------------------------------------------------------
 
 
---
+++ 100DaysOfCode/Day2.py
@@ -0,0 +1,16 @@
+# Python Primitive Data Types
+# String = anything between " "
+s = "Hello World"
+# Integer = Whole number
+i = 5
+# Float = real numbers
+f = 3.14159
+# Boolean = True or False
+b = True
+#-------------------------------------------------------------------------------
+# Subscrtpting: printing the element/char of a list/string
+# note indexing starts from 0
+print(s[0])
+print(s[-1])
+#-------------------------------------------------------------------------------
+
