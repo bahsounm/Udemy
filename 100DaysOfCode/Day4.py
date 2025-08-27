6dff40c0095081a770e45ef73d97789280c1b1a4
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-08-27 16:26:15 -0400

    working on day 4

---
+++ 100DaysOfCode/Day4.py
@@ -0,0 +1,24 @@
+# Random Module
+import random as rand
+import Day4_Mod
+
+# randint chooses a random number between a and b
+
+print(rand.randint(1,10))
+# How we can create our own module
+print(Day4_Mod.my_fav_num)
+
+print(rand.random())
+#-------------------------------------------------------------------------------
+# Offset and Appending Items to Lists
+
+province = ["Ontario", "Manitoba", "Nova Scotia", "Quebec", "British Columbia"]
+
+# this is how we can access the elements in the list. Note: it starts at 0
+print(province[0])
+
+
+
+
+
+
