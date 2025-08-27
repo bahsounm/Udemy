ef375a84776539d3a56d69771010ce331b1b5548
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-08-27 18:04:35 -0400

    completed Day 6

---
+++ 100DaysOfCode/Day6.py
@@ -0,0 +1,15 @@
+# Defining and Calling Functions 
+
+def my_func():
+    print("Hello")
+    print("Bye")
+my_func()
+
+# see hurdle site
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# While loops
+i = 0
+while i < 10:
+    print(i)
+    i+=1
+
