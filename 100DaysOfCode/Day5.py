6e74aaf812dab53fc2f0b2c8ac7f4236427035dc
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-08-27 16:57:57 -0400

    starting day 5

---
+++ 100DaysOfCode/Day5.py
@@ -0,0 +1,13 @@
+# Using a for loop with lists
+
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+
+# or
+
+for fruit in range(0, len(fruits)):
+    print(fruits[fruit])
+
+#-------------------------------------------------------------------------------
