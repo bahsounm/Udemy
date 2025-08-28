319ca5ba8dacfa3c79f9e70e13156e3170817e7d
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-08-28 13:38:43 -0400

    completed day8

--- 100DaysOfCode/Day8/Day8.py
+++ 100DaysOfCode/Day8/Day8.py
@@ -1,5 +1,5 @@
 # Caesar Cipher
-
+import art 
 # Encryption
 def encrypt(original_text, shift_amount):
     temp = list(original_text)
@@ -19,5 +19,27 @@ def decrypt(original_text, shift_amount):
     print(decrypted_text)
 
 
-def caesar():
+# encrpyt asnd decrypt are the same just the plus or minus use the idea of * by -1
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    ans = 1
+    if encode_or_decode == "decode":
+        ans = -1
+    temp = list(original_text)
+    for i in range(len(temp)):
+        temp[i] = chr(ord(temp[i]) + shift_amount*ans)
     
+    text = ''.join(temp)
+    print(f"Here is the result: {text}")
+
+
+
+print(art.art)
+while True:
+    decode_or_encode = input("Type 'encode' to encrpty or 'decode' to decrypt:\n")
+    original_text = input("Type your message:\n")
+    shift_amount = int(input("Type your shift number:\n"))
+    caesar(original_text, shift_amount, decode_or_encode)
+    nxt_step = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
+    if nxt_step == 'no':
+        break
