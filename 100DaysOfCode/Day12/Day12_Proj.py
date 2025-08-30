71e784d57b93076ce9b142636f8bd8f831614e9b
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-08-30 16:32:11 -0400

    fixing file loc

--- 100DaysOfCode/Day12/Day12_Proj.py
+++ 100DaysOfCode/Day12/Day12_Proj.py
@@ -3,25 +3,29 @@ import art
 import random as rand
 
 def num_guess_game(difficulty):
-    if difficulty == 'easy':
-        num_of_tries = 10
-    else:
+    if difficulty == 'hard':
         num_of_tries = 5
+    else:
+        num_of_tries = 10
     
     num_to_guess = rand.randint(1,100)
     guessed_correct = False
 
     while guessed_correct == False and num_of_tries > 0:
-        guess = input(f"You have {num_of_tries} attempts remaining to guess the number\nMake a Guess: ")
+        guess = int(input(f"You have {num_of_tries} attempts remaining to guess the number\nMake a Guess: "))
 
         if guess == num_to_guess:
-            guessed_correct == True
+            guessed_correct = True
             print(f"You got it! The answer was {num_to_guess}")
-        elif guess > num_to_guess:
-            print("Too High. \nGuess Again.")
-        elif guess < num_to_guess:
-            print("Too Low. \nGuess Again.")
+            continue
+        
         num_of_tries-=1
+        if num_of_tries == 0:
+            print("You've run out of guesses. You Lose")
+        elif guess > num_to_guess:
+            print("Too High.")
+        elif guess < num_to_guess:
+            print("Too Low.")
         
 print(art.title)
 print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
