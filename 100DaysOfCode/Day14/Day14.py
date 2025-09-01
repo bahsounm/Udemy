ebb8699a8a93811feef0fb94365dbc22e0392b36
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-09-01 14:08:20 -0400

    completed day 14

--- 100DaysOfCode/Day14/Day14.py
+++ 100DaysOfCode/Day14/Day14.py
@@ -2,10 +2,7 @@
 import game_data as data
 import art
 import random as rand
-
-print(art.logo)
-
-score = 0
+import os
 
 def get_option(already_used):
     option = rand.choice(data.data)
@@ -19,22 +16,41 @@ def print_question(option_a, option_b):
     print(art.vs)
     print(f"Against B: {option_b['name']}, a {option_b['description']}, from {option_b['country']}")
 
+def check_answer(a, b):
+    if a > b:
+        return "A"
+    elif a < b:
+        return "B"
+    else:
+        return None
 
 
 def higher_lower():
     winning = True
+    score = 0
     already_used = []
+    
     option_a, already_used = get_option(already_used)
     while winning:
         option_b, already_used = get_option(already_used)
-
+        
+        print(art.logo)
+        if score != 0:
+            print(f"You're right!. Current Score: {score}")
         print_question(option_a, option_b)
-
         choice = input("Who has more follwoers? Type 'A' or 'B': ")
+        
+        ans = check_answer(option_a["follower_count"], option_b["follower_count"])
 
-        if (option_a['follower_count'] > option_b["follower_count"] and choice == 'A') or (option_a['follower_count'] < option_b["follower_count"] and choice == 'B'):
-            
+        if choice == ans:
+            score +=1
+            if choice == "B":
+                option_a = option_b
+            os.system('cls')
         else:
             winning = False
+            os.system('cls')
+            print(art.logo)
+            print(f"Sorry, that's wrong. Final Score: {score}")
 
 higher_lower()
