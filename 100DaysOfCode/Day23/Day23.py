569ba5e9b885e676efebc89507565dab009b17e4
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-09-22 15:06:23 -0400

    finished Day 23

--- 100DaysOfCode/Day23/Day23.py
+++ 100DaysOfCode/Day23/Day23.py
@@ -3,11 +3,14 @@ import time
 from turtle import Screen
 from player import Player
 from scoreboard import Scoreboard
+from car_manager import CarManager
+import random as rand
 #---------------------------------------------------------------------
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.listen()
 screen.tracer(0)
+car_man = CarManager()
 #---------------------------------------------------------------------
 player = Player()
 score = Scoreboard()
@@ -15,13 +18,25 @@ score = Scoreboard()
 game_is_on = True
 
 screen.onkeypress(key="Up", fun=player.move)
-
+offset = 40
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_man.start_cars()
 
+    # check if the player has crossed the finish line
     if player.ycor() > 280:
+        # update the score, and take the turtle back to the starting position for the next level
         score.increase_score()
         player.reset_position()
+        car_man.next_level()
+    
+    for car in car_man.cars:
+        if player.distance(car) < 20:
+            score.game_over()
+            game_is_on = False
+        if car.xcor() < -330:
+            car.setx(350 + offset)
+            offset += 40
 #---------------------------------------------------------------------
 screen.exitonclick()
--- 100DaysOfCode/Day23/car_manager.py
+++ 100DaysOfCode/Day23/car_manager.py
@@ -1,5 +1,38 @@
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 10
 
+from turtle import Turtle
+import random as rand
+
 class CarManager:
-    pass
+    def __init__(self):
+        self.cars = []
+        self.multiplier = 1
+        self.car_speed = 1
+        self.generate_cars()
+
+
+    def generate_cars(self):
+        x = 40
+        for i in range(100):
+            car = Turtle()
+            car.penup()
+            car.color(rand.choice(COLORS))
+            car.shape("square")
+            car.setheading(180)
+            car.shapesize(stretch_len=2,stretch_wid=1)
+            car.goto(350 + x, rand.randint(-250,270))
+            car.speed(10)
+            self.cars.append(car)
+            x+= 40
+
+
+    def start_cars(self):
+        for car in self.cars:
+            car.forward(STARTING_MOVE_DISTANCE*self.multiplier)
+
+    def next_level(self):
+        self.car_speed += 1
+        self.multiplier *= 1.09
+        for car in self.cars:
+            car.speed(self.car_speed)
