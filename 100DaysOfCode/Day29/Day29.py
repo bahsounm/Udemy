79ce91fe1006f7b38904ddeb62f6ce0c464a39d5
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-10-08 04:24:36 +0800

    completed Day 29

---
+++ 100DaysOfCode/Day29/Day29.py
@@ -0,0 +1,88 @@
+import tkinter as tk
+import random as rand
+from tkinter import messagebox
+# ---------------------------- Function SETUP ------------------------------- #
+def save_pass():
+    website = entry_website.get()
+    email_user = entry_email_uname.get()
+    password = entry_password.get()
+
+    if website == "":
+        messagebox.showerror(message="Please Enter a Website")
+        return
+    if email_user == "":
+        messagebox.showerror(message="Please Enter an Email/Username")
+        return
+    if password == "":
+        messagebox.showerror(message="Please Enter a Password")
+        return
+
+    is_ok = messagebox.askokcancel(message="You entered: \n\nWebsite: {} \nEmail/Username: {} \nPassword: {}".format(website,email_user,password))
+    if is_ok:
+        with open("password_manager.txt", "a") as f:
+            f.write("Website: {}\nEmail: {}\nPassword: {}\n\n".format(website,email_user,password))
+        
+        entry_website.delete(0, tk.END)
+        entry_password.delete(0, tk.END)
+
+def generate_pass():
+    letters = ['a','b','c','d','e','f','g','h','i','j','j','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+           'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    symbols = ['!','#','$','%','&','(',')','*','+']
+    numbers = ['0', '1','2','3','4','5','6','7','8','9']
+
+    our_chars = []
+
+    for i in range(4):
+        our_chars.append(numbers[rand.randint(0, len(numbers)-1)])
+    for i in range(4):
+        our_chars.append(symbols[rand.randint(0, len(symbols)-1)])
+    for i in range(4):
+        our_chars.append(letters[rand.randint(0, len(letters)-1)])
+
+    password = ""
+    rand.shuffle(our_chars)
+    for i in our_chars:
+        password += i
+    entry_password.delete(0, tk.END)
+    entry_password.insert(0,password)
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# ---------------------------- Canvas ------------------------------- #
+canvas = tk.Canvas(width=200, height=200)
+lock_img = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+
+# ---------------------------- Grid SETUP ------------------------------- #
+label_website=tk.Label(text="Website:")
+label_website.grid(column=0, row=1)
+# =================================================================================
+entry_website = tk.Entry()
+entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")
+entry_website.focus() # make the cursor automatically start at this entry
+# =================================================================================
+label_email_uname = tk.Label(text="Email/Username:")
+label_email_uname.grid(column=0, row=2)
+
+entry_email_uname = tk.Entry()
+entry_email_uname.grid(column=1, row=2, columnspan=2, sticky="EW")
+entry_email_uname.insert(0, "hassunaBazuna02@gmail.com")
+# =================================================================================
+label_password = tk.Label(text="Password:")
+label_password.grid(column=0, row=3)
+
+entry_password = tk.Entry()
+entry_password.grid(column=1, row=3, sticky="EW")
+# =================================================================================
+generate_btn = tk.Button(text="Generate Password", command=generate_pass)
+generate_btn.grid(column=2, row=3, sticky="EW")
+ 
+add_btn = tk.Button(text="Add", width=35, command=save_pass)
+add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
+# =================================================================================
+window.mainloop()
