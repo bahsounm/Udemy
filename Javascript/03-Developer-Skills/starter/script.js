42bdcf7f24ba273816137b800122a88464cfec73
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-11-04 20:13:24 -0500

    some stuff

--- Javascript/03-Developer-Skills/starter/script.js
+++ Javascript/03-Developer-Skills/starter/script.js
@@ -1,3 +1,23 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// prettier is now set up soooo, when you click ctrl save, prettier will format it prettier for us
+// console.log('Hello world');
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+
+const measureKelvin = function(){
+    const measurement = {
+        type: "temp",
+        unit: "celcious",
+        value: prompt("degrees celcius: ")
+
+    }
+    const kelvin = measurement.value +273
+    return kelvin
+}
+
+console.log(measureKelvin())
